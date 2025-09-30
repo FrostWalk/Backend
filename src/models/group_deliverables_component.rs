ade7d2289cbeb932fc678f6e9e467c78ea5f45f4
Federico Frigerio <federico.frigerio@proton.me>
2025-10-01 00:10:37 +0200

    feat: add CRUD operations for group deliverables-components, including create, read, update, and delete endpoints

--- src/models/group_deliverables_component.rs
+++ src/models/group_deliverables_component.rs
@@ -3,6 +3,8 @@ use welds::WeldsModel;
 #[derive(Debug, Clone, WeldsModel)]
 #[welds(schema = "public", table = "group_deliverables_components")]
 pub struct GroupDeliverablesComponent {
+    #[welds(primary_key)]
+    pub id: i32,
     #[welds(foreign_key = "group_deliverables.group_deliverable_id")]
     pub group_deliverable_id: i32,
     #[welds(foreign_key = "group_deliverable_components.group_deliverable_component_id")]
